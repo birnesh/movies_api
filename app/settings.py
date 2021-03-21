707073663d970e4f31cf71ef601ec5c3ffd59b84
Birnesh Eswar <birnesh711@gmail.com>
2021-03-21 13:56:17 +0900

    added the postgres support

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,13 +73,21 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movies',
+        'USER': 'movies',
+        'PASSWORD': 'movies',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+db_url = os.environ.get("DATABASE_URL")
+if db_url:
+    DATABASES['default'] = dj_database_url.parse(db_url,conn_max_age=600)
+   
 
 
 # Password validation
